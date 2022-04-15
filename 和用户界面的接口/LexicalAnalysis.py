4fa9bdd46d0c45171ca397dc328c17bcb72fdcb3
Tony <75828419+TLLL123@users.noreply.github.com>
2022-04-15 20:14:13 +0800

    词法分析，LL1，画树，语义分析

---
+++ 和用户界面的接口/LexicalAnalysis.py
@@ -0,0 +1,226 @@
+class LexicalAnalysis:
+    def __init__(self,txt):
+        self.stateTransitionTable={
+            0: {0: 1, 1: 2, 2: 3, 3: 4, 4: 3, 5: 6, 6: 13, 7: 8, 8: 10, 9: 13},
+            1: {0: 1, 1: 1, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0},
+            2: {0: 13, 1: 2, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0},
+            3: {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0},
+            4: {0: 13, 1: 13, 2: 13, 3: 13, 4: 5, 5: 13, 6: 13, 7: 13, 8: 13, 9: 13},
+            5: {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0},
+            6: {0: 6, 1: 6, 2: 6, 3: 6, 4: 6, 5: 6, 6: 7, 7: 6, 8: 6, 9: 6},
+            7: {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0},
+            8: {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 9, 8: 0, 9: 0},
+            9: {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0},
+            10: {0: 11, 1: 11, 2: 13, 3: 13, 4: 13, 5: 13, 6: 13, 7: 13, 8: 13, 9: 13},
+            11: {0: 13, 1: 13, 2: 13, 3: 13, 4: 13, 5: 13, 6: 13, 7: 13, 8: 12, 9: 13},
+            12: {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0},
+            13: {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0}
+        }
+        self.txt=txt
+        self.tokens=[]
+        self.delimiters='+-*/();[]<, '
+        self.reservedWords=[
+            'PROGRAM', 'PROCEDURE', 'TYPE', 'VAR', 'IF',
+            'THEN', 'ELSE', 'FI', 'WHILE', 'DO',
+            'ENDWH', 'BEGIN', 'END', 'READ', 'WRITE',
+            'ARRAY', 'OF', 'RECORD', 'RETURN',
+            'INTEGER', 'CHAR'
+        ]
+        self.operators={
+                         '=': 'EQ',  '<': 'LT',    '+': 'PLUS',     '-': 'MINUS',
+            '*': 'TIMES','/': 'OVER','[': 'LPAREN',']': 'RPAREN',   '.': 'DOT',
+                         ';': 'SEMI',',': 'COMMA', '(': 'LMIDPAREN',')': 'RMIDPAREN'
+
+        }
+        self.errors=[]
+        self.errorLines=[]
+
+    def getCharType(self,ch):
+        if ch.isalpha(): return 0
+        if ch.isdigit(): return 1
+        if ch in self.delimiters: return 2
+        if ch == ':':return 3
+        if ch=='=': return 4
+        if ch=='{': return 5
+        if ch=='}': return 6
+        if ch=='.': return 7
+        if ch=='\'':return 8
+        return 9#其它字符
+
+    def getToken(self,state,line,buf):
+        word = "".join(buf)
+        if(state==1):
+            CapsLK=word.upper()
+            if CapsLK in self.reservedWords:
+                return (line,CapsLK,word)
+            else:
+                return (line,"ID",word)
+        if(state==2):
+            number=int(word)#恰巧SNL语言没有浮点型数
+            return (line,"CONST",number)
+        if(state==3):return (line,self.operators[word],word)
+        if(state==4):pass
+        if(state==5):return (line,"ASSIGN",word)
+        if(state==6):pass
+        if(state==7):return (line,"NOTES",word)
+        if(state==8):return (line,"DOT",word)
+        if(state==9):return (line,"UNDERANGE",word)
+        if(state==10):pass
+        if(state==11):pass
+        if(state==12):return (line,"ID",word)
+        if(state==13):return (line,"ERROR",word)
+
+    def analyze(self):
+        txt=self.txt
+        nowState=0
+        stateList=[]
+        line=1
+        charbuf=[]
+        for ch in txt:
+            chCopy=ch
+            if ch in '\t\n':
+                ch=' '
+            chtype=self.getCharType(ch)
+            nextState=self.stateTransitionTable[nowState][chtype]
+            if(nextState!=0):#当前单词尚未识别完毕
+                charbuf.append(ch)
+                nowState=nextState
+                #print(nowState)
+                stateList.append(nowState)
+            else:#已完成一个单词的识别
+                if charbuf != [' ']:#跳过空格
+                    newToken=self.getToken(nowState,line,charbuf)
+                    if(newToken[1]=="NOTES"):pass
+                    elif(newToken[1]=="ERROR"):
+                        tempString="第"+str(line)+"行"+"单词"+newToken[2]+"出现词法错误"+"\n"
+                        self.errors.append(tempString)
+                        self.errorLines.append(line)
+                    else:
+                        self.tokens.append(newToken)
+                nowState=self.stateTransitionTable[0][chtype]
+                #print(nowState)
+                stateList.append(nowState)
+                charbuf=[ch]
+            if(chCopy=='\n'):
+                line+=1
+            if(ch=='$'):
+                self.tokens.append((line,"EOF","文件结束符号，无语义信息"))
+
+        return stateList
+
+code1='''{}program pp
+type t=integer;
+var integer v1,v2,v3;
+char a1,b,c;
+array [1..20] of integer d;
+procedure f();
+   begin
+      v1 := 20 + 10;
+      if v1 = 30
+      then v2 := 30
+      else v2:=10
+      fi
+   end
+begin
+   f();
+   write(v1)
+end.'''
+
+code2='''Program p
+type t=integer;
+t1=char;
+t2=record
+        integer e1,e2;
+        char f1,f2;
+        array[1..5] of integer g1,g2;
+   end;
+var t v1,v2,v3;
+t1 v4;
+array[1..20] of integer a,b,c;
+
+procedure v1Add(t v1);
+var integer temp1;
+begin
+   temp1:=10;
+   v1:=v1+temp1;
+   write(v1)
+end
+
+procedure v2Dec(integer v2);
+var integer temp2;
+begin
+   temp2:=10;
+   v2:=v2-temp2;
+   write(v2)
+end
+
+begin
+   read(v1);
+   v1Add(10);
+   write(v1);
+   read(v1);
+   write(a[1])
+end.'''
+
+code3='''program  bubble
+var  integer  i,j,num;
+     array [1..20] of integer  a;
+procedure  q(integer num);
+var  integer i,j,k;
+     integer t;
+     
+begin
+  i:=1;
+   while i < num do
+     j:=num-i+1;
+     k:=1;
+     while k<j  do
+    	if a[k+1] < a[k]  
+        then   
+	        t:=a[k];
+		    a[k]:=a[k+1];
+		    a[k+1]:=t
+        else  temp:=0
+        fi;   
+     k:=k+1
+     endwh;
+  i:=i+1
+  endwh
+end
+
+begin
+   read(num);
+   i:=1;
+   while i<(num+1)  do
+     read(j);
+     a[i]:=j;
+     i:=i+1
+   endwh;
+   q(num);
+   i:=1;
+   while  i<(num+1) do 
+       write(a[i]);
+       i:=i+1
+   endwh
+end.'''
+codeInput=''''''#多行字符串，接收外部输入
+codeInput=code2
+if len(codeInput)==0:
+    exit()
+obj=LexicalAnalysis(txt=codeInput+'$')
+if (len(obj.errors) == 0):
+    #print("无词法错误")
+    pass
+else:
+    print(*obj.errors, sep='\n')
+#字符序列列表charList
+charList=list(obj.txt)
+#print(charList)
+#print(len(charList))
+#状态序列列表stateList，每个元素均和charList对应
+stateList=obj.analyze()
+#print(stateList)
+#print(len(stateList))
+
+#print(obj.tokens)#token序列
+#print(*obj.tokens, sep='\n')
---
+++ 和用户界面的接口/SemanticAnalysis.py
@@ -0,0 +1,373 @@
+'''
+构造全局符号表，扫描完一层后删除当前层符号表
+检查当前层内重复定义错误、
+未定义就使用错误、
+函数调用时参数类型及个数的匹配错误
+'''
+import LexicalAnalysis as tokens
+
+class Symbol:#符号表元素
+    def __init__(self,name,level,kind,type,ElemType,Low,Up):
+        self.name=name
+        self.level=level
+        self.kind=kind
+        self.type=type
+        self.ElemType=ElemType
+        self.Low=Low
+        self.Up=Up
+
+parameterDict={}#形参表
+
+#print(tokens.obj.txt)
+tokens=tokens.obj.tokens
+#print(tokens)
+SymTab=[]#元素是Sym对象
+Level=0#当前层数
+lineSkipNum=0
+
+def redefinition(name,Level):#声明阶段，判断此标识符在当前层是否重复定义
+    for Sym in SymTab:
+        if Sym.level==Level:
+            if Sym.name==name:
+                return 1
+    return 0
+
+def delNowLevel(Level):#删除当前层符号表
+    while True:
+        if SymTab[-1].level==Level:
+            SymTab.pop()
+        if SymTab[-1].level!=Level:
+            break
+
+def judgeDefine(ID):
+    for Sym in SymTab:
+        if Sym.name==ID:
+            return 1
+    return 0
+
+def getType(name):
+    if name.upper() in ['INTEGER','CHAR']:
+        return name.upper()
+    for Sym in SymTab:
+        if Sym.kind=='TYPE' and Sym.name==name:
+            return Sym.type
+
+def name2type(name):#用于检查形实参是否匹配
+    for Sym in SymTab:
+        if Sym.kind == 'VAR' and Sym.name == name:
+            return Sym.type
+    if isinstance(name, int) == 1:
+        return 'INTEGER'
+    elif isinstance(name, str) == 1:
+        return 'CHAR'
+
+def returnSymItem(name):
+    for Sym in SymTab:
+        if Sym.name==name:
+            return Sym
+
+def outFormat(content):
+    if content==None:
+        return ''
+    else:
+        return str(content)
+
+class SemanticError(Exception):
+    def __init__(self, message, lineno):
+        super().__init__(message, lineno)
+        self.message = message
+        self.lineno = lineno
+
+class SemanticChecker:#功能5,6,7,10,11,12
+    def __init__(self, *args, isTree=False):
+        self.tokenlist = args if not isTree else None
+        self.root = args if isTree else None
+        self.idx = 0
+
+    def getType(self, name):
+        pass
+
+    def getKind(self, name):
+        pass
+
+    def getElementType(self, name):
+        pass
+
+    def matchBegin(self):
+        if self.tokenlist[self.idx][1] != 'BEGIN':
+            self.idx += 1
+            return False
+        else:
+            self.idx += 1
+            return True
+
+    def matchEnd(self):
+        if self.tokenlist[self.idx][1] == 'END':
+            return True
+        return False
+
+    def checkArrayMember(self):
+        if self.getType(self.tokenlist[self.idx][2]) == 'ARRAY':
+            if self.tokenlist[self.idx + 1][2] == '[':
+                i = self.idx + 2
+                while self.tokenlist[i][1] not in ['ID', 'CONST']:
+                    i += 1
+                if self.tokenlist[i][1] == 'CONST' \
+                        or self.getType(self.tokenlist[i][2]) == 'INTEGER':
+                    return
+            message = "数组成员变量引用不合法"
+            raise SemanticError(message, self.tokenlist[self.idx][0])
+
+    def checkRecordMember(self):
+        if self.getType(self.tokenlist[self.idx][2]) == 'RECORD':
+            if self.tokenlist[self.idx + 1][2] == '.':
+                # if self.tokenlist[self.idx + 1][2] in 记录中的域
+                pass
+
+    def checkProcedureCall(self):
+        if self.tokenlist[self.idx + 1][2] == '(' \
+                and self.getKind(self.tokenlist[self.idx][2]) != 'PROCEDURE':
+            message = "过程调用语句中的标识符不是过程标识符"
+            raise SemanticError(message, self.tokenlist[self.idx][0])
+
+    def checkAssignment(self):
+        left = None
+        i = self.idx - 1
+        while self.tokenlist[i][2] not in ['begin', 'then', 'else', ';', 'do']:
+            if self.tokenlist[i][1] == 'ID':
+                left = self.tokenlist[i][2]
+            i -= 1
+        i = self.idx + 1
+        while self.tokenlist[i][1] not in ['ID', 'CONST']:
+            i += 1
+        right = self.tokenlist[i][2] if self.tokenlist[i][1] == 'ID' else self.tokenlist[i][1]
+        if self.getKind(left) != 'VAR':
+            message = "赋值语句左端不是变量标识符"
+            raise SemanticError(message, self.tokenlist[self.idx][0])
+        if self.getElementType(left) == 'INTEGER' and right == 'CONST':
+            return
+        if self.getElementType(left) != self.getElementType(right):
+            message = "赋值语句左右两边类型不相容"
+            raise SemanticError(message, self.tokenlist[self.idx][0])
+
+    def checkOperation(self):
+        left = None
+        i = self.idx - 1
+        while self.tokenlist[i][2] not in [':=', '[', '(', '+', '-', '*', '/']:
+            if self.tokenlist[i][2] == ']':
+                while self.tokenlist[i][2] != '[':
+                    i -= 1
+            if self.tokenlist[i][1] in ['ID', 'CONST']:
+                left = self.tokenlist[i][2] if self.tokenlist[i][1] == 'ID' else self.tokenlist[i][1]
+            i -= 1
+        i = self.idx + 1
+        while self.tokenlist[i][1] not in ['ID', 'CONST']:
+            i += 1
+        right = self.tokenlist[i][2] if self.tokenlist[i][1] == 'ID' else self.tokenlist[i][1]
+        if left =='CONST' and right == 'CONST':
+            return
+        elif left =='CONST' and self.getElementType(right) == 'INTEGER':
+            return
+        elif right =='CONST' and self.getElementType(left) == 'INTEGER':
+            return
+        elif self.getElementType(left) == self.getElementType(right):
+            return
+        message = "表达式中运算符的分量不一致"
+        raise SemanticError(message, self.tokenlist[self.idx][0])
+
+    def checkCmparison(self):
+        left = None
+        i = self.idx - 1
+        while self.tokenlist[i][2] not in ['while', 'if']:
+            if self.tokenlist[i][1] in ['ID', 'CONST']:
+                left = self.tokenlist[i][2] if self.tokenlist[i][1] == 'ID' else self.tokenlist[i][1]
+            i -= 1
+        i = self.idx + 1
+        while self.tokenlist[i][1] not in ['ID', 'CONST']:
+            i += 1
+        right = self.tokenlist[i][2] if self.tokenlist[i][1] == 'ID' else self.tokenlist[i][1]
+        if left =='CONST' and right == 'CONST':
+            return
+        elif left =='CONST' and self.getElementType(right) == 'INTEGER':
+            return
+        elif right =='CONST' and self.getElementType(left) == 'INTEGER':
+            return
+        elif self.getElementType(left) == self.getElementType(right):
+            return
+        message = "if和while语句的条件判断部分不是bool型"
+        raise SemanticError(message, self.tokenlist[self.idx][0])
+
+    def tokenListCheck(self):
+        self.idx = 0
+        isbegin = False
+        while self.idx < len(self.tokenlist):
+            if not isbegin:
+                if not self.matchBegin():
+                    continue
+                isbegin = True
+            if self.tokenlist[self.idx][1] == 'ID':
+                self.checkArrayMember()
+                self.checkRecordMember()
+                self.checkProcedureCall()
+            if self.tokenlist[self.idx][2] == ':=':
+                self.checkAssignment()
+            if self.tokenlist[self.idx][2] in ['+', '-', '*', '/']:
+                self.checkOperation()
+            if self.tokenlist[self.idx][2] in ['=', '<']:
+                self.checkCmparison()
+            if self.matchEnd():
+                isbegin = False
+            self.idx += 1
+
+semanticErrorFlag=0
+lineSkipNum=[]
+#print('\33[31m')#错误信息用红字输出
+for i in range(len(tokens)):
+    if(tokens[i][0] in lineSkipNum):
+        continue
+    if tokens[i][1]=='PROGRAM':
+        SymTab.append(Symbol(tokens[i+1][2],Level,'PROGRAM',None,None,None,None))
+        Level+=1
+    elif tokens[i][1]=='TYPE':
+        lineSkipNum=[tokens[i][0]]
+        flag=0
+        for j in range(i+1,len(tokens)):
+            if tokens[j][1]=='VAR' or tokens[j][1]=='PROCEDURE' or tokens[j][1]=='BEGIN':#将跨行的类型别名也计入统计
+                break
+            lineSkipNum.append(tokens[j][0])
+            if tokens[j][1]=='ID':
+                if redefinition(tokens[j][2],Level)==1:
+                    flag=1;semanticErrorFlag=1
+                    print("\33[31m第"+str(tokens[j][0])+"行，标识符"+tokens[j][2]+"重复定义，请修改错误后再进行语义分析")
+                    break
+                SymTab.append(Symbol(tokens[j][2],Level,'TYPE',tokens[j+2][1],None,None,None))
+        if flag==1:
+            break
+    elif tokens[i][1]=='VAR':
+        lineSkipNum=[tokens[i][0]]
+        nowType=None
+        if tokens[i + 1][1] != 'INTEGER' and tokens[i + 1][1] != 'CHAR':
+            for Sym in SymTab:
+                if Sym.name==tokens[i+1][2]:
+                    nowType=Sym.type
+                    break
+        else:
+            nowType=tokens[i+1][1]
+        if nowType==None:
+            semanticErrorFlag=1
+            print("\33[31m第"+str(tokens[i][0])+"行，类型"+tokens[i+1][2]+"未定义，请修改错误后再进行语义分析")
+            break
+        flag=0
+        for j in range(i+2,len(tokens)):
+            if(tokens[j][2]==';'):#统计到i后第一个;之前
+                lineSkipNum.append(tokens[j][0])
+                break
+            lineSkipNum.append(tokens[j][0])
+            if(tokens[j][1]=='ID'):
+                if redefinition(tokens[j][2],Level)==1:
+                    semanticErrorFlag = 1;flag = 1
+                    print("\33[31m第" + str(tokens[j][0]) + "行，标识符" + tokens[j][2] + "重复定义，请修改错误后再进行语义分析")
+                    break
+                SymTab.append(Symbol(tokens[j][2],Level,'VAR',nowType,None,None,None))
+        if flag==1:
+            break
+    elif tokens[i][1]=='ARRAY':
+        lineSkipNum=[]
+        for j in range(i,i+8):
+            lineSkipNum.append(tokens[j][0])
+        flag=0
+        for j in range(i+8,len(tokens)):
+            if tokens[j][2]==';':#统计到i后第一个;前为止
+                lineSkipNum.append(tokens[j][0])
+                break
+            lineSkipNum.append(tokens[j][0])
+            if tokens[j][1]=='ID':
+                if redefinition(tokens[j][2], Level) == 1:
+                    semanticErrorFlag = 1;flag = 1
+                    print("\33[31m第" + str(tokens[j][0]) + "行，标识符" + tokens[j][2] + "重复定义，请修改错误后再进行语义分析")
+                    break
+                SymTab.append(Symbol(tokens[j][2], Level, 'VAR', 'ARRAY', tokens[i + 7][1],tokens[i+2][2],tokens[i+4][2]))
+        if flag==1:
+            break
+    elif tokens[i][1]=='PROCEDURE':
+        if(redefinition(tokens[i+1][2],Level)==1):
+            semanticErrorFlag=1
+            print("\33[31m第" + str(tokens[i+1][0]) + "行，标识符" + tokens[i+1][2] + "重复定义，请修改错误后再进行语义分析")
+            break
+        SymTab.append(Symbol(tokens[i+1][2],Level,'PROCEDURE',None,None,None,None))
+        parameterDict.update({tokens[i+1][2]:[]})
+        for j in range(i+3,len(tokens)):
+            if tokens[j][2]==')':
+                break
+            if tokens[j+1][1]=='ID' and tokens[j][2]!=';':
+                parameterDict[tokens[i+1][2]].append(getType(tokens[j][2]))
+        Level+=1
+    elif getType(str(tokens[i][2])) in ['INTEGER','CHAR']:#函数参数
+        if(redefinition(tokens[i+1][2],Level)==1):
+            semanticErrorFlag=1
+            print("\33[31m第" + str(tokens[i+1][0]) + "行，标识符" + tokens[i+1][2] + "重复定义，请修改错误后再进行语义分析")
+            break
+        SymTab.append(Symbol(tokens[i+1][2],Level,'VAR',getType(tokens[i][2]),'None',None,None))
+    elif tokens[i][1]=='END':
+        #想进行正常语义分析需要保留下行语句，删了则可以输出完整符号表
+        #delNowLevel(Level)
+        Level-=1
+    elif tokens[i][1]=='BEGIN':
+        '''
+        for i in SymTab:
+            print('\33[34m{0:<15}{1:<15}{2:<15}{3:<15}{4:<15}{5:<15}{6:<15}'.format(outFormat(i.name), outFormat(i.level),
+                                                                                  outFormat(i.kind), outFormat(i.type),
+                                                                                  outFormat(i.ElemType),
+                                                                                  outFormat(i.Low), outFormat(i.Up)))
+
+        tokenlist1 = tokens
+        checker = SemanticChecker(*tokenlist1, isTree=False)
+        checker.tokenListCheck()
+        '''
+        j=i+1
+        flag=0
+        while tokens[j][1]!='END':
+            if(tokens[j][1]=='ID'):
+                if judgeDefine(tokens[j][2])==0:
+                    semanticErrorFlag=1;flag=1
+                    print("\33[31m第"+str(tokens[j][0])+"行，标识符"+tokens[j][2]+"未定义就使用，请修改错误后再继续进行语义分析")
+                    break
+                if tokens[j+1][2]=='[':
+                    if tokens[j+2][1]=='ID':#用变量访问数组，直接跳过不作分析
+                        j+=1
+                        continue
+                    if tokens[j+2][2]<returnSymItem(tokens[j][2]).Low or tokens[j+2][2]>returnSymItem(tokens[j][2]).Up:
+                        semanticErrorFlag = 1;flag=1
+                        print("\33[31m第"+str(tokens[j+1][0])+"行，数组"+str(tokens[j][2])+"下标越界,请修改错误后再继续进行语义分析")
+                        break
+                elif tokens[j][2] in parameterDict:#检查函数调用时参数的匹配问题
+                    realParList=[]
+                    k=j+2
+                    while tokens[k][2]!=')':#提取实参列表
+                        if tokens[k][1]=='ID' and judgeDefine(tokens[k][2])==1:
+                            realParList.append(name2type(tokens[k][2]))
+                        elif tokens[k][1]=='CONST':
+                            realParList.append(name2type(tokens[k][2]))
+                        k+=1
+                    if realParList!=parameterDict[tokens[j][2]]:
+                        semanticErrorFlag = 1;flag=1
+                        if len(realParList)==len(parameterDict):
+                            print("\33[31m第" + str(tokens[j][0]) + "行，调用" + str(tokens[j][2]) + "函数时形实参个数不相同，请修改后再进行语义分析")
+                        else:
+                            print("\33[31m第" + str(tokens[j][0]) + "行，调用" + str(tokens[j][2]) + "函数时形实参类型不匹配，请修改后再进行语义分析")
+                        break
+            if flag==1:
+                break
+            j+=1
+        if semanticErrorFlag==1:
+            break
+
+print("\33[34m{0:<15}{1:<15}{2:<15}{3:<15}{4:<15}{5:<15}{6:<15}".format("name", "level","kind","type","ElemType","Low","Up"))
+for i in SymTab:
+    print('\33[34m{0:<15}{1:<15}{2:<15}{3:<15}{4:<15}{5:<15}{6:<15}'.format(outFormat(i.name), outFormat(i.level),outFormat(i.kind),outFormat(i.type),outFormat(i.ElemType),outFormat(i.Low),outFormat(i.Up)))
+
+print("\33[31m形参表：")
+print(parameterDict)
+
+if semanticErrorFlag==0:
+    print("\33[30m无语义错误")
